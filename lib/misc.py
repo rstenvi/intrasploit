9aa027f09893b28740571d5604fb2a710bc44d20
Robin Stenvi <robin.stenvi@gmail.com>
2018-08-01 16:15:41 +0000

    Changed length of random ids

--- lib/misc.py
+++ lib/misc.py
@@ -8,7 +8,7 @@ import string
 import random
 
 
-def random_id(size=10, chars=string.ascii_lowercase + string.digits):
+def random_id(size=16, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 def hostname2id(hostname):
